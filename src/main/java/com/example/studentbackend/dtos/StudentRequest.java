7dd34317d7c47cfc3cfc98bfa885faaafa11a37b
VittorTrigoneDias <Vittor.Trigone.Dias@gmail.com>
2023-09-04 23:43:04 -0300

    first commit

---
+++ src/main/java/com/example/studentbackend/dtos/StudentRequest.java
@@ -0,0 +1,18 @@
+package com.example.studentbackend.dtos;
+
+
+import jakarta.validation.constraints.NotBlank;
+
+public record StudentRequest(
+    @NotBlank(message = "O Nome é obrigatório!")
+    String name, 
+    @NotBlank(message = "O Email é obrigatório!")
+    String email, 
+    @NotBlank(message = "O CPF é obrigatório!")
+    String cpf, 
+    @NotBlank(message = "O endereço é obrigatório!")
+    String endereco
+
+) {
+    
+}
---
+++ src/main/java/com/example/studentbackend/mappers/StudentMapper.java
@@ -0,0 +1,31 @@
+package com.example.studentbackend.mappers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.example.studentbackend.dtos.StudentRequest;
+import com.example.studentbackend.dtos.StudentResponse;
+import com.example.studentbackend.entities.Student;
+
+public class StudentMapper {
+    public static Student toEntity(StudentRequest request) {
+        Student student = new Student();
+        student.setName(request.name());
+        student.setEmail(request.email());
+        student.setCpf(request.cpf());
+        student.setEndereco(request.endereco());
+        return student;
+    }
+
+    public static StudentResponse toDTO(Student student) {
+        return new StudentResponse(student.getId(), student.getName(), student.getEmail(), student.getCpf(),
+                student.getEndereco());
+    }
+
+    public static List<StudentResponse> toDTOList(List<Student> students) {
+        return students.stream()
+                .map(StudentMapper::toDTO) // Mapeia cada Student para StudentResponse
+                .collect(Collectors.toList()); // Coleta os resultados em uma lista
+    }
+}
+
